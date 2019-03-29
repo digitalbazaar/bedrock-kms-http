f01ff90f9d892b94e3a36b162789db2047cf2178
Matthew Collier <mcollier@digitalbazaar.com>
2019-03-29 10:26:29 -0400

    Setup test suite.

--- test/mocha/10-http.js
+++ test/mocha/10-http.js
@@ -3,28 +3,40 @@
  */
 'use strict';
 
+const bedrock = require('bedrock');
 const https = require('https');
+const {create} = require('apisauce');
+const {config, util: {uuid}} = bedrock;
 // allow self-signed cert for tests
-const axios = require('axios').create({
-  httpsAgent: new https.Agent({
-    rejectUnauthorized: false
-  })
-});
-const {config} = require('bedrock');
-//const helpers = require('./helpers');
+const httpsAgent = new https.Agent({rejectUnauthorized: false});
+
+const baseURL = `${config.server.baseUri}/kms`;
+const api = create({baseURL});
 
 describe('bedrock-kms-http API', () => {
   describe('operations', () => {
     it('should execute a "generateKey" operation', async () => {
-      let err;
-      try {
-        //await axios.post(...);
-      } catch(e) {
-        err = e;
-      }
-      should.exist(err);
-      should.exist(err.response);
-      err.response.status.should.equal(400);
+      const operation = {
+        '@context': 'https://w3id.org/security/v2',
+        type: 'GenerateKeyOperation',
+        invocationTarget: {
+          id: 'idFoo',
+          type: 'typeFoo',
+          controller: 'controllerFoo',
+        },
+        proof: {
+          type: uuid(),
+          capability: uuid(),
+          created: uuid(),
+          jws: uuid(),
+          proofPurpose: 'capabilityInvocation',
+          verificationMethod: uuid()
+        }
+      };
+      const response = await api.post(`/foo/bar`, operation, {httpsAgent});
+      console.log('ERROR', response.data);
+      // should.exist(err.response);
+      // err.response.status.should.equal(400);
     });
     it('should fail to execute a "generateKey" operation', async () => {
     });
