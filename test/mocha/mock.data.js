0fbf4574e5e36ead9e071ba2fdc8386f5ec180f5
Mandy Venables <mandylvenables@gmail.com>
2020-07-15 17:41:03 -0400

    Condense eslint-disable max-len in mock data.

--- test/mocha/mock.data.js
+++ test/mocha/mock.data.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const data = {};
 module.exports = data;
 
@@ -7,31 +8,23 @@ const zcap0 = {
   '@context': 'https://w3id.org/security/v2',
   id: 'urn:zcap:z19vWhR8EsNbWqvazp5bg6BTu',
   controller: 'did:key:z6Mkkt1BWYLPAAXwYBwyVHAZkL94tgT8QbQv2SUxeW1U3DaG',
-  // eslint-disable-next-line max-len
   referenceId: 'did:key:z6MkkrtV7wnBpXKBtiZjxaSghCo8ttb5kZUJTk8bEwTTTYvg#z6MkkrtV7wnBpXKBtiZjxaSghCo8ttb5kZUJTk8bEwTTTYvg-key-capabilityInvocation',
   allowedAction: 'sign',
   invocationTarget: {
-    // eslint-disable-next-line max-len
     id: 'https://bedrock.localhost:18443/kms/keystores/z1AAWWM7Zd4YyyV3NfaCqFuzQ/keys/z19wxodgv1UhrToQMvSxGhQG6',
     type: 'Ed25519VerificationKey2018',
-    // eslint-disable-next-line max-len
     verificationMethod: 'did:key:z6MkkrtV7wnBpXKBtiZjxaSghCo8ttb5kZUJTk8bEwTTTYvg#z6MkkrtV7wnBpXKBtiZjxaSghCo8ttb5kZUJTk8bEwTTTYvg'
   },
-  // eslint-disable-next-line max-len
   invoker: 'did:key:z6MkfV83MxASJKXim3eBPoLCDiWDseYUaW84qbVF9k3ngdfg#z6MkfV83MxASJKXim3eBPoLCDiWDseYUaW84qbVF9k3ngdfg',
-  // eslint-disable-next-line max-len
   parentCapability: 'https://bedrock.localhost:18443/kms/keystores/z1AAWWM7Zd4YyyV3NfaCqFuzQ/keys/z19wxodgv1UhrToQMvSxGhQG6',
   proof: {
     type: 'Ed25519Signature2018',
     created: '2020-02-27T21:22:48Z',
-    // eslint-disable-next-line max-len
     verificationMethod: 'did:key:z6MkkrtV7wnBpXKBtiZjxaSghCo8ttb5kZUJTk8bEwTTTYvg#z6MkkrtV7wnBpXKBtiZjxaSghCo8ttb5kZUJTk8bEwTTTYvg',
     proofPurpose: 'capabilityDelegation',
     capabilityChain: [
-      // eslint-disable-next-line max-len
       'https://bedrock.localhost:18443/kms/keystores/z1AAWWM7Zd4YyyV3NfaCqFuzQ/keys/z19wxodgv1UhrToQMvSxGhQG6'
     ],
-    // eslint-disable-next-line max-len
     jws: 'eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..bWt6_Q65omg8rE44a_1dzWFGcFQbUrVqZ_hnAqIKlWSQ1HpTSV6OyhAQfBlVhPCsrVplqC8oVEJmp4UWqy6gCw'
   }
 };
