ff4639c239a1d9b52ee8bc54faaa2d6b42a59f6e
Dave Longley <dlongley@digitalbazaar.com>
2021-07-19 17:47:28 -0400

    Use updated bedrock-meter* deps.

--- test/test.js
+++ test/test.js
@@ -4,12 +4,23 @@
 const bedrock = require('bedrock');
 require('bedrock-https-agent');
 require('bedrock-kms-http');
+require('bedrock-meter');
 require('bedrock-security-context');
-
+require('bedrock-meter');
+const {handlers} = require('bedrock-meter-http');
 // this is responsible for providing the `ssm-v1` key store
 require('bedrock-ssm-mongodb');
 
 require('bedrock-test');
 require('bedrock-karma');
+
+bedrock.events.on('bedrock.init', async () => {
+  /* Handlers need to be added before `bedrock.start` is called. These are
+  no-op handlers to enable meter usage without restriction */
+  handlers.setCreateHandler({handler: ({}) => {}});
+  handlers.setUpdateHandler({handler: ({}) => {}});
+  handlers.setRemoveHandler({handler: () => {}});
+  handlers.setUseHandler({handler: () => {}});
+});
 
 bedrock.start();
