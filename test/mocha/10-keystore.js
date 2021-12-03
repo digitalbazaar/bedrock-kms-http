dcb54bd784b563bb2c31081818a220735ce57464
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2021-12-03 08:40:34 -0500

    Add test for getting key decription and remove tests for findKeystore.

--- test/mocha/10-keystore.js
+++ test/mocha/10-keystore.js
@@ -4,7 +4,9 @@
 'use strict';
 
 const bedrock = require('bedrock');
-const {CapabilityAgent} = require('@digitalbazaar/webkms-client');
+const brHttpsAgent = require('bedrock-https-agent');
+const {CapabilityAgent, KmsClient, KeystoreAgent} =
+  require('@digitalbazaar/webkms-client');
 const helpers = require('./helpers');
 const {agent} = require('bedrock-https-agent');
 const {httpClient, DEFAULT_HEADERS} = require('@digitalbazaar/http-client');
@@ -237,6 +239,47 @@ describe('bedrock-kms-http API', () => {
         ]);
         result.id.should.equal(keystore.id);
       });
+      it('gets public key decription', async () => {
+        const secret = '34f2afd1-34ef-4d46-a998-cdc5462dc0d2';
+        const handle = 'bobKey';
+        const capabilityAgent = await CapabilityAgent.fromSecret(
+          {secret, handle});
+        const {id: keystoreId} = await helpers.createKeystore(
+          {capabilityAgent});
+        const {httpsAgent} = brHttpsAgent;
+        const kmsClient = new KmsClient({keystoreId, httpsAgent});
+
+        let keystoreAgent;
+        try {
+          keystoreAgent = new KeystoreAgent(
+            {capabilityAgent, keystoreId, kmsClient});
+        } catch(e) {
+          assertNoError(e);
+        }
+        const key = await keystoreAgent.generateKey({type: 'asymmetric'});
+
+        const url = key.id;
+        const headers = await signCapabilityInvocation({
+          url, method: 'get',
+          headers: DEFAULT_HEADERS,
+          capability: 'urn:zcap:root:' + encodeURIComponent(url),
+          invocationSigner: capabilityAgent.getSigner(),
+          capabilityAction: 'read'
+        });
+
+        let err;
+        let result;
+        try {
+          result = await httpClient.get(url, {agent, headers});
+        } catch(e) {
+          err = e;
+        }
+        assertNoError(err);
+        should.exist(result);
+        result.data.should.have.keys([
+          '@context', 'id', 'type', 'publicKeyMultibase'
+        ]);
+      });
       it('gets a keystore with ipAllowList', async () => {
         const secret = 'b07e6b31-d910-438e-9a5f-08d945a5f676';
         const handle = 'testKey1';
@@ -288,87 +331,6 @@ describe('bedrock-kms-http API', () => {
         err.data.type.should.equal('NotAllowedError');
       });
     }); // get keystore config
-
-    // FIXME: skipped while considering removal of keystore query
-    describe.skip('find keystore configs', () => {
-      it('finds a keystore', async () => {
-        const secret = ' b0f43022-3af1-4f22-ae55-19d70582087a';
-        const handle = 'testKey1';
-        const capabilityAgent = await CapabilityAgent.fromSecret(
-          {secret, handle});
-
-        const referenceId = 'urn:uuid:4f398f8f-505a-4609-a9df-761f01f4d18b';
-        const keystore = await helpers.createKeystore({
-          capabilityAgent, referenceId});
-        let err;
-        let result;
-        try {
-          result = await helpers.findKeystore({
-            controller: keystore.controller, referenceId});
-        } catch(e) {
-          err = e;
-        }
-        assertNoError(err);
-        should.exist(result);
-        result.should.have.keys([
-          'controller', 'id', 'referenceId', 'sequence', 'kmsModule', 'meterId'
-        ]);
-        result.id.should.equal(keystore.id);
-        result.controller.should.equal(keystore.controller);
-        result.referenceId.should.equal(keystore.referenceId);
-      });
-      it('finds a keystore with ipAllowList', async () => {
-        const secret = ' b0f43022-3af1-4f22-ae55-19d70582087a';
-        const handle = 'testKey1';
-        const capabilityAgent = await CapabilityAgent.fromSecret(
-          {secret, handle});
-
-        const ipAllowList = ['127.0.0.1/32'];
-        const referenceId = 'urn:uuid:7c8b5e04-bbeb-4267-bdf3-b8ea425f9e32';
-        const keystore = await helpers.createKeystore({
-          capabilityAgent, ipAllowList, referenceId});
-        let err;
-        let result;
-        try {
-          result = await helpers.findKeystore({
-            controller: keystore.controller, referenceId});
-        } catch(e) {
-          err = e;
-        }
-        assertNoError(err);
-        should.exist(result);
-        result.should.have.keys([
-          'controller', 'id', 'ipAllowList', 'referenceId', 'sequence',
-          'kmsModule', 'meterId'
-        ]);
-        result.id.should.equal(keystore.id);
-        result.controller.should.equal(keystore.controller);
-        result.referenceId.should.equal(keystore.referenceId);
-        result.ipAllowList.should.eql(ipAllowList);
-      });
-      it('find returns null with ipAllowList and invalid source IP',
-        async () => {
-          const secret = ' b0f43022-3af1-4f22-ae55-19d70582087a';
-          const handle = 'testKey1';
-          const capabilityAgent = await CapabilityAgent.fromSecret(
-            {secret, handle});
-
-          const ipAllowList = ['8.8.8.8/32'];
-          const referenceId = 'urn:uuid:17884c10-218d-4398-8f85-58a20cfc4bab';
-          const keystore = await helpers.createKeystore({
-            capabilityAgent, ipAllowList, referenceId});
-          let err;
-          let result;
-          try {
-            result = await helpers.findKeystore({
-              controller: keystore.controller, referenceId});
-          } catch(e) {
-            err = e;
-          }
-          assertNoError(err);
-          should.equal(result, null);
-        });
-    });
 
     describe('update keystore config', () => {
       it('updates a keystore config', async () => {
