3fa95640fa9eb77c4f7ade4e5846c06625c9f90b
Dave Longley <dlongley@digitalbazaar.com>
2022-04-05 19:28:49 -0400

    Fix linting errors.

--- test/mocha/31-operations-hmac.js
+++ test/mocha/31-operations-hmac.js
@@ -1,6 +1,7 @@
 /*!
  * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
  */
+import * as bedrock from '@bedrock/core';
 import * as helpers from './helpers.js';
 import {httpsAgent} from '@bedrock/https-agent';
 import {createRequire} from 'module';
